61ec009d04b8fa0cf1d0b116075682116204ff46
Katsu <messyaikei456@gmail.com>
2022-08-21 22:35:25 +0530

    update

---
+++ mod4/js/script.js
@@ -0,0 +1,17 @@
+let arr= new Array();
+arr[0]="john";
+arr[1]="marcos";
+arr[2]="jimmy";
+arr[3]="peter";
+arr[4]="vincenzo";
+for (let i = 0; i < arr.length; i++) {
+    let ele = arr[i];
+    if(ele.charAt(0)=="j" || ele.charAt(0)=="J"){
+        console.log("Goodbye "+ele);
+    }
+    else{
+        console.log("Hello "+ele);
+    }
+
+    
+}
